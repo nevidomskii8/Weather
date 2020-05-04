c0fc9ef83b069819c540516f6e33c08663ed9a30
Yakov <nevidomskiy8@gmail.com>
2020-05-04 13:08:00 +0300

    modified options

--- src/components/CitiesSelect.js
+++ src/components/CitiesSelect.js
@@ -1,23 +1,17 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { dataCities } from '../container/DataArrays'
 import { toggleCity } from '../redux/action'
 import { CityOption } from './CityOption'
 
 function CitiesSelect() {
-	const reducerProps = useSelector(state => state),
- 	 coordinates = reducerProps.appDate.selectedCity,
- 	 dispatch = useDispatch(),
+	 const dispatch = useDispatch(),
 	 selectHandler = event => {
 		dispatch(toggleCity(dataCities[event.target.value]))
-	 },
-	 indexCity = Object.values(dataCities).indexOf(coordinates),
-	 city = Object.keys(dataCities)[indexCity]
-
+	 }
 	return (
 		<div className="input-field col s12 m6">
 			<select className="icons" onChange={selectHandler}>
-				<option defaultValue={city}>{city}</option>
 				<CityOption/>
 			</select>
     	</div>
--- src/components/CityOption.js
+++ src/components/CityOption.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { dataCities } from '../container/DataArrays'
+import { useSelector } from 'react-redux'
 
 
 export const  CityOption = () => {
-    return  Object.keys(dataCities).map(key => {
-        return (
-                    <option value={key}>{key} </option>
-                )
-    })
+	const reducerProps = useSelector(state => state),
+     coordinates = reducerProps.appDate.selectedCity,
+     indexCity = Object.values(dataCities).indexOf(coordinates),
+	 city = Object.keys(dataCities)[indexCity]
+
+    return  (
+       <> 
+            <option defaultValue={city}>{city}</option>
+            {
+                Object.keys(dataCities).map(key => {
+                    return (
+                            <option value={key}>{key} </option>
+                        )
+                    })
+            }
+       </>
+    )
 }
